efad9c769d040d0eb3d27ed0d0c23eb07c7df158
AdamMakiewiczDelivr <a.makiewicz@wearedelivr.com>
2020-04-19 10:05:51 +0200

    no message

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello React please work 2.0
+          Hello React please work 3.0
         </p>
         <a
           className="App-link"
