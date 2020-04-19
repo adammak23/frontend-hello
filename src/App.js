bd56795e6b6cd9f5aa514fe3a04012fc8925dc42
AdamMakiewiczDelivr <a.makiewicz@wearedelivr.com>
2020-04-19 09:55:19 +0200

    branch commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello React
+          Hello React please work
         </p>
         <a
           className="App-link"
